dc6ef40c04e72a6cf589945bfd424f70c441d063
hwang434 <inqbator@knou.ac.kr>
2021-08-23 17:22:26 +0900

    회원가입, 로그인창 이동 로직 추가

--- app/src/main/java/com/goodee/cando_app/views/MainFragment.kt
+++ app/src/main/java/com/goodee/cando_app/views/MainFragment.kt
@@ -1,11 +1,17 @@
 package com.goodee.cando_app.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.goodee.cando_app.R
+import com.goodee.cando_app.databinding.FragmentMainBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +24,22 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private val TAG: String = "로그"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
+        Log.d(TAG,"MainFragment - onCreateView() called")
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,R.layout.fragment_main,container,false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // 이벤트 등록
+        binding.registerButton.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_mainFragment_to_registerFragment)
+        }
+        binding.loginButton.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+        }
+
+        return binding.root
     }
 }
