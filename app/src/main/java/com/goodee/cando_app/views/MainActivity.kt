583c684d830e6b620db3f6dec416f0271eea4620
hwang434 <inqbator@knou.ac.kr>
2021-08-23 16:07:33 +0900

    라이프사이클 메소드 오버라이딩해서 로그 추가

--- app/src/main/java/com/goodee/cando_app/views/MainActivity.kt
+++ app/src/main/java/com/goodee/cando_app/views/MainActivity.kt
@@ -2,17 +2,48 @@ package com.goodee.cando_app.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.goodee.cando_app.R
 import com.goodee.cando_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "로그"
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"MainActivity - onCreate() called")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
 
+    override fun onStart() {
+        Log.d(TAG,"MainActivity - onStart() called")
+        super.onStart()
+    }
 
+    override fun onResume() {
+        Log.d(TAG,"MainActivity - onResume() called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG,"MainActivity - onPause() called")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG,"MainActivity - onStop() called")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG,"MainActivity - onRestart() called")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG,"MainActivity - onDestroy() called")
+        super.onDestroy()
     }
 }
