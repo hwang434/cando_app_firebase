6bdcf4fde106a9686038ea1a7908d77ec6fcd527
hwang434 <inqbator@knou.ac.kr>
2021-08-26 18:21:31 +0900

    데이터 바인딩 설정 완료

--- app/src/main/java/com/goodee/cando_app/views/RegisterFragment.kt
+++ app/src/main/java/com/goodee/cando_app/views/RegisterFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.goodee.cando_app.R
+import com.goodee.cando_app.databinding.FragmentRegisterBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,12 +20,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class registerFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false)
 
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return binding.root
     }
 }
