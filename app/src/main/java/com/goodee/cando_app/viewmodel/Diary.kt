7e493d12ee942df1cfa9f638b764f2cb5055b770
hwang434 <inqbator@knou.ac.kr>
2021-09-06 12:15:02 +0900

    다이어리용 dto 추가

---
+++ app/src/main/java/com/goodee/cando_app/viewmodel/Diary.kt
@@ -0,0 +1,10 @@
+package com.goodee.cando_app.viewmodel
+
+import java.util.*
+
+data class Diary(
+    var diaryTitle: String,
+    var diaryWriter: String,
+    var diaryWriteDate: Date,
+    var diaryUpdateDate: Date
+    )
