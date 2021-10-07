aa57e12578ef334876265fa90192e206d1168f2b
hwang434 <inqbator@knou.ac.kr>
2021-10-07 11:10:40 +0900

    Prevent click button to double click
    Make a abstract class which extends View.OnClickListner.
    Measure between clicked time, if the time difference is bigger than 1second than stop the click event.

--- app/src/main/java/com/goodee/cando_app/views/LoginFragment.kt
+++ app/src/main/java/com/goodee/cando_app/views/LoginFragment.kt
@@ -1,16 +1,11 @@
 package com.goodee.cando_app.views
 
-import android.app.ProgressDialog
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import android.util.Log
-import android.view.KeyEvent
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
@@ -18,6 +13,7 @@ import androidx.navigation.fragment.findNavController
 import com.goodee.cando_app.R
 import com.goodee.cando_app.database.RealTimeDatabase
 import com.goodee.cando_app.databinding.FragmentLoginBinding
+import com.goodee.cando_app.listener.SingleClickListner
 
 class LoginFragment : Fragment() {
     private val TAG: String = "로그"
@@ -29,6 +25,7 @@ class LoginFragment : Fragment() {
     ): View? {
         Log.d(TAG,"LoginFragment - onCreateView() called")
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login,container, false)
+        setEvent()
 
         return binding.root
     }
@@ -38,51 +35,51 @@ class LoginFragment : Fragment() {
             view?.findNavController()?.navigate(R.id.action_loginFragment_to_findMember)
         }
         // 로그인 버튼 이벤트처리
-        binding.buttonLoginLoginbutton.setOnClickListener {
-            Log.d(TAG,"LoginFragment - loginButton is activated")
-            val imm: InputMethodManager?
-            binding.progressbarLoginLoading.visibility = View.VISIBLE
+        binding.buttonLoginLoginbutton.setOnClickListener(object: SingleClickListner() {
+            override fun onSingleClick(view: View?) {
+                Log.d(TAG,"LoginFragment - loginButton is activated")
+                val imm: InputMethodManager?
 
-            // 아이디가 비었거나 Blank거나 null일 때
-            if (binding.textviewLoginIdinput.text.isNullOrBlank() || binding.textviewLoginIdinput.text.isEmpty()) {
-                Toast.makeText(requireActivity(),"아이디를 확인해주세요.",Toast.LENGTH_SHORT).show()
+                // 아이디가 비었거나 Blank거나 null일 때
+                if (binding.textviewLoginIdinput.text.isNullOrBlank() || binding.textviewLoginIdinput.text.isEmpty()) {
+                    Toast.makeText(requireActivity(),"아이디를 확인해주세요.",Toast.LENGTH_SHORT).show()
 
-                // 포커스가 되어 있는 키보드와 showSoftInput 메소드의 view가 일치해야 키보드가 뜹니다.
-                // 포커스가 되어 있지 않으면 키보드가 뜨지 않습니다.
-                binding.textviewLoginIdinput.requestFocus()
-                imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.showSoftInput(binding.textviewLoginIdinput,0)
-            } else if (binding.edittextLoginPasswordinput.text.isNullOrBlank() || binding.edittextLoginPasswordinput.text.isEmpty()) {
-                Toast.makeText(requireActivity(),"비밀번호를 확인해주세요.",Toast.LENGTH_SHORT).show()
+                    binding.textviewLoginIdinput.requestFocus()
+                    imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.showSoftInput(binding.textviewLoginIdinput,0)
+                } else if (binding.edittextLoginPasswordinput.text.isNullOrBlank() || binding.edittextLoginPasswordinput.text.isEmpty()) {
+                    Toast.makeText(requireActivity(),"비밀번호를 확인해주세요.",Toast.LENGTH_SHORT).show()
 
-                binding.edittextLoginPasswordinput.requestFocus()
-                imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                imm.showSoftInput(binding.edittextLoginPasswordinput,0)
-            } else {
-                // 정규식을 만족했으므로 존재하는 데이터인지 확인.
-                val database = RealTimeDatabase.getDatabase()
-                database.child("users")
-                    .child(binding.textviewLoginIdinput.text.toString())
-                    .get()
-                    .addOnSuccessListener { info ->
-                        Log.i(TAG, "onCreateView:${info.child("password").value} ")
-                        val password = info.child("password").value
+                    binding.edittextLoginPasswordinput.requestFocus()
+                    imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.showSoftInput(binding.edittextLoginPasswordinput,0)
+                } else {
+                    // 정규식을 만족했으므로 존재하는 데이터인지 확인.
+                    binding.progressbarLoginLoading.visibility = View.VISIBLE
+                    val database = RealTimeDatabase.getDatabase()
+                    database.child("users")
+                        .child(binding.textviewLoginIdinput.text.toString())
+                        .get()
+                        .addOnSuccessListener { info ->
+                            Log.i(TAG, "onCreateView:${info.child("password").value} ")
+                            val password = info.child("password").value
 
-                        if (password != null) {
-                            if (password.equals(binding.edittextLoginPasswordinput.text.toString())) {
-                                Toast.makeText(requireContext(), "${binding.textviewLoginIdinput.text.toString()}님 환영합니다.",Toast.LENGTH_SHORT).show()
-                                findNavController().navigate(R.id.action_loginFragment_to_diaryFragment)
+                            if (password != null) {
+                                if (password.equals(binding.edittextLoginPasswordinput.text.toString())) {
+                                    Toast.makeText(requireContext(), "${binding.textviewLoginIdinput.text.toString()}님 환영합니다.",Toast.LENGTH_SHORT).show()
+                                    findNavController().navigate(R.id.action_loginFragment_to_diaryFragment)
+                                } else {
+                                    Toast.makeText(requireContext(), "비밀번호가 틀렸습니다.",Toast.LENGTH_SHORT).show()
+                                }
                             } else {
-                                Toast.makeText(requireContext(), "비밀번호가 틀렸습니다.",Toast.LENGTH_SHORT).show()
+                                Toast.makeText(requireContext(), "존재하지 않는 회원입니다..",Toast.LENGTH_SHORT).show()
                             }
-                        } else {
-                            Toast.makeText(requireContext(), "존재하지 않는 회원입니다..",Toast.LENGTH_SHORT).show()
-                        }
 
-                        binding.progressbarLoginLoading.visibility = View.INVISIBLE
-                    }
+                            binding.progressbarLoginLoading.visibility = View.INVISIBLE
+                        }
+                }
             }
-        }
+        })
 
         // 키보드 엔터 누르면 클릭 이벤트 실행
         binding.edittextLoginPasswordinput.setOnKeyListener { v, keyCode, event ->
