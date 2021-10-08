62bc3cedc656160f9dcddeb2f6e28135986ffb32
hwang434 <inqbator@knou.ac.kr>
2021-10-08 23:44:30 +0900

    이메일로 가입하기
    이메일로 가입하기 성공.
    이메일이 같으면 가입이 안되는데, 기본키로 설정한 게 없는데 왜 그런지 모르겠음.
    확인해봐야겠다.

--- app/src/main/java/com/goodee/cando_app/views/RegisterFragment.kt
+++ app/src/main/java/com/goodee/cando_app/views/RegisterFragment.kt
@@ -10,16 +10,32 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.goodee.cando_app.R
 import com.goodee.cando_app.api.DuplicateCheckService
+import com.goodee.cando_app.database.RealTimeDatabase
 import com.goodee.cando_app.databinding.FragmentRegisterBinding
+import com.goodee.cando_app.viewmodel.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class registerFragment : Fragment() {
     private val TAG: String = "로그"
+    private var isExistId = false
     private lateinit var binding: FragmentRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize firebase auth
+        auth = Firebase.auth
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -59,55 +75,59 @@ class registerFragment : Fragment() {
                 val imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.showSoftInput(binding.edittextRegisterEmailinput, 0)
             } else {
-                if (checkId() == true) {
+                if (isExistId) {
                     Toast.makeText(requireActivity(), "이미 존재하는 아이디입니다.",Toast.LENGTH_LONG).show()
                 } else {
                     // 회원 가입 시키기
+                    val email = binding.edittextRegisterEmailinput.text.toString()
+                    val name = binding.edittextRegisterNameinput.text.toString()
+                    val password = binding.edittextRegisterPasswordinput.text.toString()
+                    val phone = binding.edittextPhonePhoneinput.text.toString()
+                    val user = User(email, name, password, phone)
+
+                    auth.createUserWithEmailAndPassword(email
+                        ,password)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                RealTimeDatabase.getDatabase().child("Users").child(FirebaseAuth.getInstance().currentUser!!.uid)
+                                    .setValue(user)
+                                    .addOnCompleteListener {
+                                        if (it.isSuccessful) {
+                                            Toast.makeText(requireActivity(), "Register Success", Toast.LENGTH_SHORT).show()
+                                            findNavController().navigate(R.id.action_registerFragment_to_diaryFragment)
+                                        } else {
+                                            Toast.makeText(requireActivity(), "Register Fail", Toast.LENGTH_SHORT).show()
+                                        }
+                                    }
+                                    .addOnFailureListener {
+                                        Toast.makeText(requireActivity(), "Failure", Toast.LENGTH_SHORT).show()
+                                    }
+                            }
+                        }
                 }
             }
         }
 
         // DuplicateCheckService로 해당하는 id를 가진 멤버 존재하는지 확인
         binding.buttonRegisterDuplicatecheck.setOnClickListener {
-            var isExistId: Boolean? = checkId()
+            val database = RealTimeDatabase.getDatabase()
+
             if (binding.edittextRegisterIdinput.text.isNullOrBlank() || binding.edittextRegisterIdinput.text.isNullOrEmpty()) {
                 Toast.makeText(requireActivity(),"아이디를 확인해주세요",Toast.LENGTH_SHORT).show()
                 binding.edittextRegisterIdinput.requestFocus()
                 val imm = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.showSoftInput(binding.edittextRegisterIdinput,0)
             } else {
+                
                 if (isExistId == true) {
                     Toast.makeText(requireActivity(), "이미 존재하는 아이디입니다.",Toast.LENGTH_LONG).show()
                 } else if (isExistId == false) {
                     Toast.makeText(requireActivity(), "사용하셔도 좋은 아이디입니다.", Toast.LENGTH_SHORT).show()
                     // 중복 체크 완료됐다는 로직이 들어가야함.
-                } else {
-                    Toast.makeText(requireActivity(), "인터넷 연결이 안됐습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
         }
 
         return binding.root
-    }
-
-    fun checkId(): Boolean? {
-        val retrofit = DuplicateCheckService.create()
-        val duplicateCheckService = retrofit.create(DuplicateCheckService::class.java)
-        val id = binding.edittextRegisterIdinput.text.toString()
-        var isExist: Boolean? = null
-
-//        duplicateCheckService.isUserExist().enqueue(object: Callback<Boolean> {
-//            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
-//                Log.d(TAG,"registerFragment - onResponse() called")
-//                Log.d(TAG,"${response.body()}")
-//            }
-//            override fun onFailure(call: Call<Boolean>, t: Throwable) {
-//                Log.d(TAG,"registerFragment - onFailure() called")
-//                Log.d(TAG,"${t.stackTrace}")
-//            }
-//        })
-
-        Log.d(TAG,"registerFragment - isExist : $isExist")
-        return isExist
     }
 }
