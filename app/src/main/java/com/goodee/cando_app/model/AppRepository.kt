e214bfa7984424aec208e4244682eddb401ab817
hwang434 <inqbator@knou.ac.kr>
2021-10-18 20:42:45 +0900

    쓸모 없는 파일들 삭제 및 리팩토링

--- app/src/main/java/com/goodee/cando_app/model/AppRepository.kt
+++ app/src/main/java/com/goodee/cando_app/model/AppRepository.kt
@@ -23,9 +23,11 @@ import org.json.JSONObject
 class AppRepository(val application: Application) {
     private val TAG: String = "로그"
     private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
     private val _userLiveData: MutableLiveData<FirebaseUser> = MutableLiveData()
     val userLiveData: LiveData<FirebaseUser>
         get() = _userLiveData
+
     private val _diaryLivedata: MutableLiveData<List<DiaryDto>> = MutableLiveData()
     val diaryLiveData: LiveData<List<DiaryDto>>
         get() = _diaryLivedata
@@ -35,10 +37,10 @@ class AppRepository(val application: Application) {
 
         RealTimeDatabase.getDatabase().child("Diary").get().addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                val map = task.result?.value as Map<String, Map<String, String>>
-
+                val map = task.result?.value as Map<String, Map<String, String>>        // Map<식별자, 글<>>
+                val list = map.values                                                   // 글 모음
                 val diaryList = mutableListOf<DiaryDto>()
-                val list = map.values
+
                 list.forEach { it ->
                     val author = it.get("author")
                     val title = it.get("title")
@@ -57,10 +59,7 @@ class AppRepository(val application: Application) {
         Log.d(TAG,"AppRepository - register() called")
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(ContextCompat.getMainExecutor(application.applicationContext)) { task ->
             if (task.isSuccessful) _userLiveData.postValue(firebaseAuth.currentUser)
-            // 지워야할 거
-            else {
-                Toast.makeText(application, "Register Fail.", Toast.LENGTH_SHORT).show()
-            }
+            else Toast.makeText(application, "Register Fail.", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -73,22 +72,15 @@ class AppRepository(val application: Application) {
 
     fun writeDiary(diaryDto: DiaryDto) {
         Log.d(TAG,"AppRepository - writeDiary() called")
-        val key = RealTimeDatabase.getDatabase().child("Diary").push().key
+        val key = RealTimeDatabase.getDatabase().child("Diary").push().key  // 주식별자 뽑기
         val diary = HashMap<String, DiaryDto>()
-        if (key == null) {
-            diary.put("1", diaryDto)
-        } else {
-            diary.put(key, diaryDto)
-        }
+        if (key != null) diary.put(key, diaryDto)
+        else throw Exception("AppRepository - writeDiary() key is null")
 
         val firebaseDatabase = RealTimeDatabase.getDatabase()
-        firebaseDatabase.child("Diary/${key}").setValue(diaryDto).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Log.d(TAG,"AppRepository - writeDiary() is Sueccessful.")
-            }
-            else
-                Log.d(TAG,"AppRepository - writeDiary() is Fail.")
+        firebaseDatabase.child("Diary/${key}").setValue(diaryDto).addOnCompleteListener { task ->       // Diary/${key}에 글 저장하기
+            if (task.isSuccessful) Toast.makeText(application, "글 작성에 성공했습니다.", Toast.LENGTH_SHORT).show()
+            else Toast.makeText(application, "글 작성에 실패했습니다.", Toast.LENGTH_SHORT).show()
         }
-
     }
 }
