b1fad46bdd27955d40f183d0f704637c3e31d37a
hwang434 <inqbator@knou.ac.kr>
2022-04-28 01:10:41 +0900

    코드 수정

--- app/src/main/java/com/goodee/cando_app/database/RealTimeDatabase.kt
+++ app/src/main/java/com/goodee/cando_app/database/RealTimeDatabase.kt
@@ -5,10 +5,8 @@ import com.google.firebase.database.FirebaseDatabase
 import java.lang.Exception
 
 object RealTimeDatabase {
-    private val mDatabase: DatabaseReference = FirebaseDatabase.getInstance().getReference()
-    public fun getDatabase(): DatabaseReference {
-        if (mDatabase == null) throw Exception("Database is null")
-
+    private val mDatabase: DatabaseReference = FirebaseDatabase.getInstance().reference
+    fun getDatabase(): DatabaseReference {
         return mDatabase
     }
 }
