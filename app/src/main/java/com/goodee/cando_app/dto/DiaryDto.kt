4e5e4e1053d8112b5c9875dc15bd6d8d4809f518
hwang434 <inqbator@knou.ac.kr>
2022-07-21 12:30:22 +0900

    다이어리 dto 수정 게시글 작성할 때, 먼저 dno를 받아야 하므로 dno는 변할 필요가 있음.

--- app/src/main/java/com/goodee/cando_app/dto/DiaryDto.kt
+++ app/src/main/java/com/goodee/cando_app/dto/DiaryDto.kt
@@ -3,4 +3,4 @@ package com.goodee.cando_app.dto
 import java.io.Serializable
 
 
-data class DiaryDto(val dno: String = "0", val title: String = "제목", val content: String = "내용", val author: String = "관리자", val date: Long = 0) : Serializable
+data class DiaryDto(var dno: String = "0", val title: String = "제목", val content: String = "내용", val author: String = "관리자", val date: Long = 0) : Serializable
